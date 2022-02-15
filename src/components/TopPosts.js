c1793822a456ea2b573f0dca4f356090dceac336
Antti <anttituomola8@gmail.com>
2022-02-15 14:47:56 +0200

    sort feature

--- src/components/TopPosts.js
+++ src/components/TopPosts.js
@@ -2,10 +2,39 @@ import React from "react"
 import StoryCard from "./StoryCard"
 
 export default class TopPosts extends React.Component{
-    render() {
-         let posts = this.props.posts.map(el => {
-             return <StoryCard post={el} key={el.id}/>
+    state = {
+        sortBy: ""
+    }
+
+    sort = (event) => {
+        this.setState({
+            sortBy: event.target.value
         })
-        return posts
+    }
+
+    render() {
+        let sortedPosts = []
+        let sorter = this.state.sortBy
+        if (sorter === "by") {
+            sortedPosts = [].concat(this.props.posts).sort((a,b) => a[sorter].localeCompare(b[sorter]))
+        } else {
+            sortedPosts = [].concat(this.props.posts).sort((a,b) => a[sorter] < b[sorter] ? 1 : -1)
+        }
+
+
+        let posts = sortedPosts.map(el => {
+            return <StoryCard post={el} key={el.id}/>
+        })
+        return <>
+        <div id="sort" onChange={this.sort}>
+            <input type="radio" value="by" name="sort" /> By author
+            <input type="radio" value="time" name="sort" /> By date
+            <input type="radio" value="score" name="sort" /> By score
+        </div>
+        <div id="filter">
+            
+        </div>
+        <div id="results">{posts}</div>
+        </>
     }
 }
